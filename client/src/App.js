0fb73d12f3730110983eb66524181adcf94c8007
Jackson Leb <jacksonleb@Jacksons-MBP.lan>
2022-06-05 17:07:02 -0400

    started front end development

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
+    </>
+  );
+}
+
+export default App;
