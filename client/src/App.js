2b5c84b44452ef93fa082b04eeef10a9b257ec25
Jackson Leb <jacksonleb@Jacksons-MBP.lan>
2022-06-09 18:03:31 -0400

    added redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,13 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+//Redux imports
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
+  <Provider store={store}>
     <>
     <Router>
       <Navbar />
@@ -17,6 +21,7 @@ function App() {
       </Routes>
     </Router>
     </>
+  </Provider>
   );
 }
 
